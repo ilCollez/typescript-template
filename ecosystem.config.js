d9b0509912d8a47f8fb68f7319280440bb889ae7
Marco Colletti <marco.colletti34@gmail.com>
2022-01-24 22:52:09 +0100

    removed watch in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
         {
             name: 'TypeScript App',
             script: 'dist/main.js',
-            watch: 'dist',
-            watch_delay: 4000,
             env: {
                 NODE_ENV: 'production'
             }
